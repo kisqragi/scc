a5013cf3d43076f1149ee816b80284efa72d7260
kisqragi <kisaragi.sec@gmail.com>
2020-10-19 10:23:04 +0000

    Add + and - operators

--- main.c
+++ main.c
@@ -2,14 +2,34 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
-    return 1;
-  }
+    if (argc != 2) {
+        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+        return 1;
+    }
 
-  printf("  .globl main\n");
-  printf("main:\n");
-  printf("  mov $%d, %%rax\n", atoi(argv[1]));
-  printf("  ret\n");
-  return 0;
+    char *p = argv[1];
+
+    printf("  .globl main\n");
+    printf("main:\n");
+    printf("  mov $%d, %%rax\n", atoi(argv[1]));
+    printf("  mov $%ld, %%rax\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add $%ld, %%rax\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub $%ld, %%rax\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpected character: '%c'\n", *p);
+        return 1;
+    }
+    printf("  ret\n");
+    return 0;
 }
