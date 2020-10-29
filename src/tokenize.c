43527e171a89282ebc8ca5a51b9f18e85d9c32dd
kisqragi <kisaragi.sec@gmail.com>
2020-10-29 12:28:17 +0000

    Add \a, \b, \t, \n \v, \f, \r and \e

--- src/tokenize.c
+++ src/tokenize.c
@@ -1,4 +1,5 @@
 #include "scc.h"
+#include <stdlib.h>
 
 // Input string
 static char *current_input;
@@ -114,16 +115,49 @@ static void convert_keywords(Token *tok) {
     }
 }
 
-static Token *read_string_literal(char *start) {
-    char *p = start + 1;
+static char read_escaped_char(char *p) {
+    switch (*p) {
+        case 'a': return  '\a';
+        case 'b': return  '\b';
+        case 't': return  '\t';
+        case 'n': return  '\n';
+        case 'v': return  '\v';
+        case 'f': return  '\f';
+        case 'r': return  '\r';
+        // '\e' -> GNU C extention.
+        case 'e': return  27;
+        default: return *p;
+    }
+}
+
+static char *string_literal_end(char *p) {
+    char *start = p;
     for (; *p != '"'; p++) {
         if (*p == '\n' || *p == '\0')
             error_at(start, "unclosed string literal");
+        if (*p == '\\')
+            p++;
+    }
+    return p;
+}
+
+static Token *read_string_literal(char *start) {
+    char *end = string_literal_end(start + 1);
+    char *buf = calloc(1, end - start);
+    int len = 0;
+
+    for (char *p = start + 1; p < end;) {
+        if (*p == '\\') {
+            buf[len++] = read_escaped_char(p + 1);
+            p += 2;
+        } else {
+            buf[len++] = *p++;
+        }
     }
 
-    Token *tok = new_token(TK_STR, start, p+1);
-    tok->ty = array_of(ty_char, p - start);
-    tok->str = strndup(start+1, p-start-1);
+    Token *tok = new_token(TK_STR, start, end+1);
+    tok->ty = array_of(ty_char, len + 1);
+    tok->str = buf;
     return tok;
 }
 
