ce207cace3ac10d9b0b3d7d8b6034a10fe1fc20e
kisqragi <kisaragi.sec@gmail.com>
2020-11-26 08:29:26 +0000

    Use 32 bit registers for char, short and int

--- src/codegen.c
+++ src/codegen.c
@@ -155,19 +155,33 @@ static void gen_expr(Node *node) {
     gen_expr(node->lhs);
     pop("%rdi");
 
+    char *ax, *di;
+
+    if (node->lhs->ty->kind == TY_LONG || node->rhs->ty->kind == TY_LONG ||
+        node->lhs->ty->base || node->rhs->ty->base) {
+        ax = "%rax";
+        di = "%rdi";
+    } else {
+        ax = "%eax";
+        di = "%edi";
+    }
+
     switch (node->kind) {
-        case ND_ADD: println("    add %%rdi, %%rax"); return;
-        case ND_SUB: println("    sub %%rdi, %%rax"); return;
-        case ND_MUL: println("    imul %%rdi, %%rax"); return;
+        case ND_ADD: println("    add %s, %s", di, ax); return;
+        case ND_SUB: println("    sub %s, %s", di, ax); return;
+        case ND_MUL: println("    imul %s, %s", di, ax); return;
         case ND_DIV:
-            println("    cqo");
-            println("    idiv %%rdi");
+            if (node->lhs->ty->size == 8 || node->rhs->ty->size == 8)
+                println("    cqo");
+            else
+                println("    cdq");
+            println("    idiv %s", di);
             return;
         case ND_EQ:
         case ND_NE:
         case ND_LT:
         case ND_LE:
-            println("    cmp %%rdi, %%rax");
+            println("    cmp %s, %s", di, ax);
 
             if (node->kind == ND_EQ) println("    sete %%al");
             if (node->kind == ND_NE) println("    setne %%al");
